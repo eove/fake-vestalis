d5fdbaa1dea4e80945b9d618759a62aba1b5a72e
Xavier Detant <1016863+FaustXVI@users.noreply.github.com>
2023-06-21 16:10:16 +0200

    Allow to fake signature time

--- static/fake-vestalis.js
+++ static/fake-vestalis.js
@@ -3,7 +3,13 @@ document.getElementById("server").value = location.hostname;
 const connect = async () => {
     const target = document.getElementById("serial").value;
     const server = document.getElementById("server").value;
-    const now = new Date().toISOString() // should be done server side, done here for testing purpose only
+    const dateValue = document.getElementById("date").value;
+    let now;
+    if (dateValue) {
+        now = new Date(dateValue).toISOString();
+    } else {
+        now = new Date().toISOString();
+    } // should be done server side, done here for testing purpose only
     let signature_response = await fetch(`/sign/${target}/${now}`);
     let signature = await signature_response.json();
     document.getElementById("eo-connect").src = `https://${server}:8000/?target=${target}&timestamp=${now}&token=${signature}`
