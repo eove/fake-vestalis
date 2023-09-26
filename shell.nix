f86cce5d118c15222af435b7cc9e1854b3ff3a59
Xavier Detant <1016863+FaustXVI@users.noreply.github.com>
2023-09-26 17:30:52 +0200

    Use same version as signaling server

--- shell.nix
+++ shell.nix
@@ -1,7 +1,7 @@
 {}:
 let
-  rust-overlay = (import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/9651f0beee6e7a9783cc02eac722854851c65ae7.tar.gz"));
-  pkgs = (import (fetchTarball("https://github.com/NixOS/nixpkgs/archive/22.11.tar.gz")) {
+  rust-overlay = (import (builtins.fetchTarball "https://github.com/oxalica/rust-overlay/archive/b91d162355e88de89b379f3d6a459ade92704474.tar.gz"));
+  pkgs = (import (fetchTarball("https://github.com/NixOS/nixpkgs/archive/23.05.tar.gz")) {
     overlays = [ rust-overlay ];
   });
   secrets = if builtins.pathExists ./secrets.nix then import ./secrets.nix else { ICE_SERVERS_URL="create secrets.nix file";};
@@ -9,7 +9,7 @@ in
 pkgs.mkShell ({
   buildInputs = with pkgs; [
     (pkgs.rust-bin.stable.latest.default.override {
-      extensions = ["rust-src"];
+      extensions = ["rust-src" "llvm-tools-preview"];
     })
     pkg-config
     openssl
