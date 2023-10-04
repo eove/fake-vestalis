6d706a476d10b3393ac6779b1c4f62d95087cf4d
Xavier Detant <1016863+FaustXVI@users.noreply.github.com>
2023-10-04 10:15:58 +0200

    A bit of design

--- static/fake-vestalis.js
+++ static/fake-vestalis.js
@@ -6,8 +6,7 @@ let server;
     console.log(server)
 })()
 
-const connect = async () => {
-    const target = document.getElementById("serial").value;
+const connect = async (target) => {
     let signature_response = await fetch(`/sign/${target}`);
     let {signature, data, nonce} = await signature_response.json();
     document.getElementById("eo-connect").src = `https://${server}/view/?data=${data}&signature=${signature}&nonce=${nonce}`
